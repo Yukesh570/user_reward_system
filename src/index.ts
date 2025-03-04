f029431dc59003419ddefa1f8385734c73971c05
yukesh570 <yukesh.maha10@gmail.com>
2025-03-04 12:53:20 +0545

    port change

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Router } from "express";
 import { initializeDB } from "./db";
 import { router } from "./routes/routes";
-const port = 8000;
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.json()); // This should be before any routes
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { userRoute } from "./userRoute";
 import { rewardRoute } from "./rewardRoute";
 // import { rewardLogRoute } from './rewardLogRoute';
-// import { transactionLogRoute } from './transactionLogRoute';
 import { rewardLogRoute } from "./rewardLogRoute";
 import { authRoute } from "./auth/auth";
 // import { rewardRoute } from './rewardRoute';
@@ -11,7 +10,6 @@ const router = express.Router();
 
 router.use("/reward", rewardRoute());
 router.use("/rewardLog", rewardLogRoute());
-// router.get('transactionLog',transactionLogRoute())
 router.use("/user", userRoute());
 router.use("/auth", authRoute());
 
