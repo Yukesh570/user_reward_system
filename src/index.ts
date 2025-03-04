db21d351ef8fad17b7c445a9037eabe9d108d7ab
yukesh570 <yukesh.maha10@gmail.com>
2025-03-04 16:10:29 +0545

    changes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json()); // This should be before any routes
 
 app.use("/api", router);
+app.get("/",(req,res)=>{
+    res.send("working");
+})
 
 app.listen(port, async () => {
   await initializeDB(), console.log(`listening on port ${port}`);
